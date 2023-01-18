90bbb5965ae7ef5ea5588fb3a10dd8dc017830d0
Jalil.Martinez <jalilmazatl@gmail.com>
2023-01-17 22:57:03 -0600

    service para agregar empleado

--- empleadosApp/src/main/java/com/example/demo/controller/EmpleadoController.java
+++ empleadosApp/src/main/java/com/example/demo/controller/EmpleadoController.java
@@ -1,5 +1,31 @@
 package com.example.demo.controller;
 
-public class EmpleadoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.demo.dto.EmpleadoDTO;
+import com.example.demo.service.EmpleadoService;
+@Controller
+public class EmpleadoController {
+	@Autowired
+	public EmpleadoService empleadoService;
+	
+	@PutMapping("/guardarEmpleado")
+	public ResponseEntity <Integer> guardarPersona(@RequestBody EmpleadoDTO data){
+		Integer id = 0;
+		try {
+			id=this.empleadoService.guardarEmpleado(data);
+		}catch(Exception e){
+			System.out.println("Error");
+		}
+		if(id==0) {
+			return new ResponseEntity<>(id,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<>(id,HttpStatus.OK);
+	}
+	
 }
--- empleadosApp/src/main/java/com/example/demo/service/EmpleadoService.java
+++ empleadosApp/src/main/java/com/example/demo/service/EmpleadoService.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
-public class EmpleadoService {
+import com.example.demo.dto.EmpleadoDTO;
+import com.example.demo.entity.Empleado;
+import com.example.demo.repository.EmpleadoRepository;
 
+public class EmpleadoService {
+private EmpleadoRepository empleadoRepository;
+	
+	public EmpleadoService(EmpleadoRepository empleadoRepository) {
+		this.empleadoRepository = empleadoRepository;
+	}
+	
+	public Integer guardarEmpleado(EmpleadoDTO empleadoN ) {
+		Empleado empleado = new Empleado();
+		empleado.setGender_id(empleadoN.getGender_id());
+		empleado.setJob_id(empleadoN.getJob_id());
+		empleado.setName(empleadoN.getName());
+		empleado.setLast_name(empleadoN.getLast_name());
+		empleado.setBirthdate(empleadoN.getBirthdate());
+		
+		empleado = empleadoRepository.save(empleado);
+		return empleado.getId();
+	}
+	
 }
