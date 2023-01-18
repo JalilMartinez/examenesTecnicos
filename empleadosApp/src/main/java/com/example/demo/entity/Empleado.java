27edbe973771750404931692f678a2e462b78109
Jalil.Martinez <jalilmazatl@gmail.com>
2023-01-17 22:21:57 -0600

    Creacion de proyecto con service empleado

---
+++ empleadosApp/src/main/java/com/example/demo/entity/Empleado.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="persona", schema="public")
+public class Empleado {
+
+	
+	
+}
