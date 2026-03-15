f4d811ee5030c56ba50139d082e3c06b797a84f3
jalil <jalilmazatl@gmail.com>
2026-03-15 08:13:06 -0700

    se suben proyectos de examenes tecnicos

---
+++ examGlobal2/data-processor-api/src/main/java/com/api/data_processor_api/DataProcessorApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.data_processor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessorApiApplication.class, args);
+	}
+
+}
